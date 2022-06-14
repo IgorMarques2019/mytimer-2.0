8e6405d2bcb6a360b282a280bf70d9f7e7ad36b4
Igor Marques <49289661+IgorMarques2019@users.noreply.github.com>
2022-06-14 17:05:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+// change theme
+let changeTheme = document.body,
+  btnTheme = document.querySelector('.btn-theme');
+
+function theme() {
+  changeTheme.classList.toggle('dark')
+  if (changeTheme.classList.contains('dark')) {
+    btnTheme.setAttribute('src', './icons/light_mode.svg')
+  } else {
+    btnTheme.setAttribute('src', './icons/mode_night.svg')
+  }
+}
+
+// ------------------- TIMER -------------------
+
+
+
+let play = document.querySelector(".btn-start")
+let iconBtn = document.querySelector(".btn-start i")
+
+let minuteIn = document.querySelector('.Minutes'),
+  second = document.querySelector('.Seconds'),
+  minute = 24,
+  sec = 60,
+  progress = 0,
+  desc = 0;
+
+
+function start() {
+  iconBtn.className = "fa-solid fa-pause"
+  play.setAttribute('onclick', 'pause()')
+
+  intervalo = setInterval(() => {
+    sec--
+    clock()
+
+    if (sec == 0 && minute == 0) {
+      window.close("./index.html")
+      if (changeTheme.classList.contains('dark')) {
+        window.open("./dark.html")
+      } else {
+        window.open("./light.html")
+      }
+    }
+
+    if (sec == 0) {
+      sec = 60
+      minute--
+    }
+
+
+
+  }, 1000)
+
+
+
+}
+
+
+function pause() {
+  clearInterval(intervalo)
+  iconBtn.className = "fa-solid fa-play"
+  play.setAttribute('onclick', 'start()')
+
+}
+
+function clock() {
+  minuteIn.innerHTML = minute < 10 ? `0${minute}` : minute;
+  second.innerHTML = sec < 10 ? `0${sec}` : sec;
+}
